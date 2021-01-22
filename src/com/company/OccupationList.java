9d14b1f3217651ece0094bf447710dcf4f4e41fb
Rafael Perez <rafaelandresph@gmail.com>
2021-01-21 19:15:48 -0500

    Added a loadArray method to read the text file and insert into a new array.

--- src/com/company/OccupationList.java
+++ src/com/company/OccupationList.java
@@ -1,5 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class OccupationList {
+
+    public static void loadArray(Occupation[] occ) throws Exception {
+
+        int i;
+
+        String inCOS;
+        String inTitle;
+        int inEmployment;
+        int inSalary;
+
+        java.io.File occFile = new java.io.File("occupations.txt");
+
+        Scanner inFile = new Scanner(occFile);
+
+        for ( i = 0; i < 1000; i++){
+
+            inCOS = inFile.nextLine();
+            inTitle = inFile.nextLine();
+            inEmployment = Integer.parseInt(inFile.nextLine());
+            inSalary = Integer.parseInt(inFile.nextLine());
+
+            occ[i] = new Occupation(inCOS, inTitle, inEmployment, inSalary);
+
+        }
+        inFile.close();
+    }
 
 }
