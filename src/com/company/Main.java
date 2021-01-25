fbf314f9a437bde4d23fcacb760010a5c369dde3
Rafael Perez <rafaelandresph@gmail.com>
2021-01-24 21:38:44 -0500

    Only added name, date, and title of the project.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+/* Rafael Rodriguez
+   Professor: Chuck Herbert
+   January 24th, 2021
+   Programming Assignment: Multiple classes
+ */
 public class Main {
 
     public static void main(String[] args) throws Exception{
