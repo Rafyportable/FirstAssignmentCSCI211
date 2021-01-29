4f4461d04d54014fabd7658a519f8504589369d4
Rafael Perez <rafaelandresph@gmail.com>
2021-01-28 23:09:13 -0500

    Turn into a menu to allow users to pick which they rather do with the text files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,54 @@
 package com.company;
 
+import java.util.Scanner;
+
 /* Rafael Rodriguez
    Professor: Chuck Herbert
-   January 24th, 2021
+   January 28th, 2021
    Programming Assignment: Multiple classes
  */
+
 public class Main {
 
     public static void main(String[] args) throws Exception{
 	// write your code here
         OccupationList type = new OccupationList();
+        Scanner s = new Scanner(System.in);
 
-        type.typeHere();
+        int select;
 
+        System.out.println("*****************************");
+        System.out.println("HELLO THERE");
+        System.out.println("*****************************");
+        System.out.println("Welcome to the Occupational Memory System (OMS)");
+        System.out.println("*****************************");
+        System.out.println("Here are your options: ");
+        System.out.println("*****************************");
+
+        while(true) {
+
+            System.out.println("\n1: Print the entire list");
+            System.out.println("\n2: Total number of occupations");
+            System.out.println("\n3: The average salary of occupations");
+            System.out.println("\n4: Look for an occupation based on SOC");
+            System.out.println("\n5: Exit");
+
+            select = s.nextInt();
+
+            if (select == 1) {
+                type.print();
+            } else if (select == 2) {
+                System.out.println("Here's the total amount of occupations according to our data \n");
+                type.totalOccupation();
+            } else if (select == 3) {
+                System.out.println("Here's the average salary according to our data \n");
+                type.avgSalary();
+            } else if (select == 4) {
+                type.typeHere();
+            } else if (select == 5) {
+                System.out.println("Thank you for using this program! Adios");
+                break;
+            }
+        }
     }
 }
